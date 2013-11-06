f5c58777c729c9a45017a864bd4cf8c173199f45
Tom Buchok <tom@buchok.com>
2013-11-06 09:15:18 -0500

    adds command line options to control litmus testing

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-var LitmusTest = require('./lib/litmus-test')
+var path = require('path')
+  , fs = require('fs')
+  , LitmusTest = require('./lib/litmus-test')
   , program = require('commander')
+  , readable = process.stdin
 ;
 
 program
   .version('0.0.1')
-  .option('-t, --testId <n>', 'Test ID for re-testing emails')
+  .option('-t, --testId <n>', 'test id for re-testing emails')
+  .option('-i, --imagePath [value]', 'images file path')
+  .option('-f, --htmlFile [value]', 'html email file to test')
   .parse(process.argv)
 ;
 
-process.stdin
+
+if (program.imagePath) {
+  var imagesDir = path.join(process.cwd(), program.imagePath);
+  if (!fs.existsSync(imagesDir))
+    return console.error('ERR: Image file path does not exist. Update and try again.')
+}
+
+if (program.htmlFile) {
+  var htmlFile = path.join(process.cwd(), program.htmlFile);
+  if (!fs.existsSync(htmlFile))
+    return console.error('ERR: HTML file path does not exist. Update and try again.')
+  readable = fs.createReadStream(htmlFile);
+}
+
+readable
   .pipe(new LitmusTest(program))
   .pipe(process.stdout)
 ;
+
